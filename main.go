ab764634c70546ec63342850339aaad178fcb7a0
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-06-07 19:51:55 +0900

    golint

--- main.go
+++ main.go
@@ -21,83 +21,85 @@ var (
 )
 
 const (
-	SEE_MASK_NOCLOSEPROCESS = 0x00000040
+	_SEE_MASK_NOCLOSEPROCESS = 0x00000040
 )
 
 const (
-	ERROR_BAD_FORMAT = 11
+	_ERROR_BAD_FORMAT = 11
 )
 
 const (
-	SE_ERR_FNF             = 2
-	SE_ERR_PNF             = 3
-	SE_ERR_ACCESSDENIED    = 5
-	SE_ERR_OOM             = 8
-	SE_ERR_DLLNOTFOUND     = 32
-	SE_ERR_SHARE           = 26
-	SE_ERR_ASSOCINCOMPLETE = 27
-	SE_ERR_DDETIMEOUT      = 28
-	SE_ERR_DDEFAIL         = 29
-	SE_ERR_DDEBUSY         = 30
-	SE_ERR_NOASSOC         = 31
+	_SE_ERR_FNF             = 2
+	_SE_ERR_PNF             = 3
+	_SE_ERR_ACCESSDENIED    = 5
+	_SE_ERR_OOM             = 8
+	_SE_ERR_DLLNOTFOUND     = 32
+	_SE_ERR_SHARE           = 26
+	_SE_ERR_ASSOCINCOMPLETE = 27
+	_SE_ERR_DDETIMEOUT      = 28
+	_SE_ERR_DDEFAIL         = 29
+	_SE_ERR_DDEBUSY         = 30
+	_SE_ERR_NOASSOC         = 31
 )
 
 type (
-	DWORD     uint32
-	HANDLE    uintptr
-	HINSTANCE HANDLE
-	HKEY      HANDLE
-	HWND      HANDLE
-	ULONG     uint32
-	LPCTSTR   uintptr
-	LPVOID    uintptr
+	dword     uint32
+	hinstance syscall.Handle
+	hkey      syscall.Handle
+	hwnd      syscall.Handle
+	ulong     uint32
+	lpctstr   uintptr
+	lpvoid    uintptr
 )
 
+// SHELLEXECUTEINFO struct
 type SHELLEXECUTEINFO struct {
-	cbSize         DWORD
-	fMask          ULONG
-	hwnd           HWND
-	lpVerb         LPCTSTR
-	lpFile         LPCTSTR
-	lpParameters   LPCTSTR
-	lpDirectory    LPCTSTR
+	cbSize         dword
+	fMask          ulong
+	hwnd           hwnd
+	lpVerb         lpctstr
+	lpFile         lpctstr
+	lpParameters   lpctstr
+	lpDirectory    lpctstr
 	nShow          int
-	hInstApp       HINSTANCE
-	lpIDList       LPVOID
-	lpClass        LPCTSTR
-	hkeyClass      HKEY
-	dwHotKey       DWORD
-	hIconOrMonitor HANDLE
-	hProcess       HANDLE
+	hInstApp       hinstance
+	lpIDList       lpvoid
+	lpClass        lpctstr
+	hkeyClass      hkey
+	dwHotKey       dword
+	hIconOrMonitor syscall.Handle
+	hProcess       syscall.Handle
 }
 
-func ShellExecuteAndWait(hwnd HWND, lpOperation, lpFile, lpParameters, lpDirectory string, nShowCmd int) error {
-	var lpctstrVerb, lpctstrParameters, lpctstrDirectory LPCTSTR
+// ShellExecuteAndWait is version of ShellExecuteEx which want process
+func ShellExecuteAndWait(hwnd hwnd, lpOperation, lpFile, lpParameters, lpDirectory string, nShowCmd int) error {
+	var lpctstrVerb, lpctstrParameters, lpctstrDirectory lpctstr
 	if len(lpOperation) != 0 {
-		lpctstrVerb = LPCTSTR(unsafe.Pointer(syscall.StringToUTF16Ptr(lpOperation)))
+		lpctstrVerb = lpctstr(unsafe.Pointer(syscall.StringToUTF16Ptr(lpOperation)))
 	}
 	if len(lpParameters) != 0 {
-		lpctstrParameters = LPCTSTR(unsafe.Pointer(syscall.StringToUTF16Ptr(lpParameters)))
+		lpctstrParameters = lpctstr(unsafe.Pointer(syscall.StringToUTF16Ptr(lpParameters)))
 	}
 	if len(lpDirectory) != 0 {
-		lpctstrDirectory = LPCTSTR(unsafe.Pointer(syscall.StringToUTF16Ptr(lpDirectory)))
+		lpctstrDirectory = lpctstr(unsafe.Pointer(syscall.StringToUTF16Ptr(lpDirectory)))
 	}
 	i := &SHELLEXECUTEINFO{
-		fMask:        SEE_MASK_NOCLOSEPROCESS,
+		fMask:        _SEE_MASK_NOCLOSEPROCESS,
 		hwnd:         hwnd,
 		lpVerb:       lpctstrVerb,
-		lpFile:       LPCTSTR(unsafe.Pointer(syscall.StringToUTF16Ptr(lpFile))),
+		lpFile:       lpctstr(unsafe.Pointer(syscall.StringToUTF16Ptr(lpFile))),
 		lpParameters: lpctstrParameters,
 		lpDirectory:  lpctstrDirectory,
 		nShow:        nShowCmd,
 	}
-	i.cbSize = DWORD(unsafe.Sizeof(*i))
+	i.cbSize = dword(unsafe.Sizeof(*i))
 	return ShellExecuteEx(i)
 }
 
+// ShellExecuteEx is Windows API
 func ShellExecuteEx(pExecInfo *SHELLEXECUTEINFO) error {
 	ret, _, _ := procShellExecuteEx.Call(uintptr(unsafe.Pointer(pExecInfo)))
-	if ret == 1 && pExecInfo.fMask&SEE_MASK_NOCLOSEPROCESS != 0 {
+	if ret == 1 && pExecInfo.fMask&_SEE_MASK_NOCLOSEPROCESS != 0 {
 		s, e := syscall.WaitForSingleObject(syscall.Handle(pExecInfo.hProcess), syscall.INFINITE)
 		switch s {
 		case syscall.WAIT_OBJECT_0:
@@ -111,29 +113,29 @@ func ShellExecuteEx(pExecInfo *SHELLEXECUTEINFO) error {
 	errorMsg := ""
 	if pExecInfo.hInstApp != 0 && pExecInfo.hInstApp <= 32 {
 		switch int(pExecInfo.hInstApp) {
-		case SE_ERR_FNF:
+		case _SE_ERR_FNF:
 			errorMsg = "The specified file was not found"
-		case SE_ERR_PNF:
+		case _SE_ERR_PNF:
 			errorMsg = "The specified path was not found"
-		case ERROR_BAD_FORMAT:
+		case _ERROR_BAD_FORMAT:
 			errorMsg = "The .exe file is invalid (non-Win32 .exe or error in .exe image)"
-		case SE_ERR_ACCESSDENIED:
+		case _SE_ERR_ACCESSDENIED:
 			errorMsg = "The operating system denied access to the specified file"
-		case SE_ERR_ASSOCINCOMPLETE:
+		case _SE_ERR_ASSOCINCOMPLETE:
 			errorMsg = "The file name association is incomplete or invalid"
-		case SE_ERR_DDEBUSY:
+		case _SE_ERR_DDEBUSY:
 			errorMsg = "The DDE transaction could not be completed because other DDE transactions were being processed"
-		case SE_ERR_DDEFAIL:
+		case _SE_ERR_DDEFAIL:
 			errorMsg = "The DDE transaction failed"
-		case SE_ERR_DDETIMEOUT:
+		case _SE_ERR_DDETIMEOUT:
 			errorMsg = "The DDE transaction could not be completed because the request timed out"
-		case SE_ERR_DLLNOTFOUND:
+		case _SE_ERR_DLLNOTFOUND:
 			errorMsg = "The specified DLL was not found"
-		case SE_ERR_NOASSOC:
+		case _SE_ERR_NOASSOC:
 			errorMsg = "There is no application associated with the given file name extension"
-		case SE_ERR_OOM:
+		case _SE_ERR_OOM:
 			errorMsg = "There was not enough memory to complete the operation"
-		case SE_ERR_SHARE:
+		case _SE_ERR_SHARE:
 			errorMsg = "A sharing violation occurred"
 		default:
 			errorMsg = fmt.Sprintf("Unknown error occurred with error code %v", pExecInfo.hInstApp)
@@ -338,7 +340,6 @@ func server() int {
 			return m.Exit
 		}
 	}
-	return 0
 }
 
 func main() {
